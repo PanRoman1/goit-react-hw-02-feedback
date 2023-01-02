6c8af6739549de5cfcf73455241b473ad2bb7fba
PanRoman1 <panchuk1985@ukr.net>
2023-01-03 01:31:19 +0200

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,66 @@
-import { Feedback } from './Feedback';
+import { Component } from 'react';
 import { Layout } from './Layout';
 
-export const App = () => {
-  return (
-    <Layout>
-      <Feedback />
-    </Layout>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedback = type => {
+    this.setState(prevState => ({ [type]: prevState[type] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const total = Object.values(this.state);
+    return total.reduce((prevTotal, total) => prevTotal + total, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.ceil((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    return (
+      <Layout>
+        <section>
+          <h1>Please leave feelback</h1>
+          <button
+            type="button"
+            name="good"
+            onClick={() => this.addFeedback('good')}
+          >
+            Good
+          </button>
+          <button
+            type="button"
+            name="neutral"
+            onClick={() => this.addFeedback('neutral')}
+          >
+            Neutral
+          </button>
+          <button
+            type="button"
+            name="bad"
+            onClick={() => this.addFeedback('bad')}
+          >
+            Bad
+          </button>
+        </section>
+        <section>
+          <h2>Statistics</h2>
+          <ul>
+            <li>Good: {this.state.good}</li>
+            <li>Neutral: {this.state.neutral}</li>
+            <li>Bad: {this.state.bad}</li>
+            <li>Total: {this.countTotalFeedback()}</li>
+            <li>
+              Positive feedback: {this.countPositiveFeedbackPercentage()}%
+            </li>
+          </ul>
+        </section>
+      </Layout>
+    );
+  }
+}
--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,26 +1,71 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-export class Feedback extends Component {
-  state = { good: 0, neutral: 0, bad: 0 };
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedback = type => {
+    this.setState(prevState => ({ [type]: prevState[type] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const values = Object.values(this.state);
+
+    let total = 0;
+
+    for (const value of values) {
+      total += value;
+    }
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.ceil((this.state.good / this.countTotalFeedback) * 100);
+  };
 
   render() {
     return (
       <>
         <section>
           <h1>Please leave feelback</h1>
-          <button>Good</button>
-          <button>Neutral</button>
-          <button>Bad</button>
+          <button
+            type="button"
+            name="good"
+            onClick={() => this.addFeedback('good')}
+          >
+            Good
+          </button>
+          <button
+            type="button"
+            name="neutral"
+            onClick={() => this.addFeedback('neutral')}
+          >
+            Neutral
+          </button>
+          <button
+            type="button"
+            name="bad"
+            onClick={() => this.addFeedback('bad')}
+          >
+            Bad
+          </button>
         </section>
         <section>
           <h2>Statistics</h2>
           <ul>
-            <li>Good: 0</li>
-            <li>Neutral: 0</li>
-            <li>Bad: 0</li>
+            <li>Good: {this.state.good}</li>
+            <li>Neutral: {this.state.neutral}</li>
+            <li>Bad: {this.state.bad}</li>
+            <li>Total: {this.countTotalFeedback}</li>
+            <li>Positive feedback: {this.countPositiveFeedbackPercentage}%</li>
           </ul>
         </section>
       </>
     );
   }
 }
+
+export default Feedback;
